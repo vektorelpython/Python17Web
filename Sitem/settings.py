017741d58b696f9decd246592911f2471f94322b
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-01-03 19:04:13 +0200

    03012020_3

--- Sitem/settings.py
+++ Sitem/settings.py
@@ -108,7 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '' 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 os.path.join(BASE_DIR,'assets'))
 
